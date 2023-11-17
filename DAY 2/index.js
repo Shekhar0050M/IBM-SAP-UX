c1a048a5c46cae499aa6d64235c05e06107a5619
Shekhar0050M <shekhar0050m@gmail.com>
2023-11-17 08:35:01 +0530

    assignment 1

--- DAY 2/index.js
+++ DAY 2/index.js
@@ -11,12 +11,18 @@ sap.ui.define([
     }).placeAt("content");
 });
 
-sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
-	"use strict";
 
-	XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
-		oView.placeAt("content");
-	});
+
+sap.ui.require([
+    "sap/m/Button",
+    "sap/m/MessageToast"
+], function (Button, MessageToast) {
+    "use strict";
+    
+    new Button({
+        text: "Ready...",
+        press: function () {
+            MessageToast.show("Hello World!");
+        }
+    }).placeAt("content");
 });
