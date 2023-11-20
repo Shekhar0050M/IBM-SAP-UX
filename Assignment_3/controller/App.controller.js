60f97dece5f516793c9f0f2f2de036f572039716
Shekhar0050M <shekhar0050m@gmail.com>
2023-11-20 21:43:02 +0530

    Assignment_3

--- Assignment_3/controller/App.controller.js
+++ Assignment_3/controller/App.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  ], function(Controller) {
     "use strict";
-    return Controller.extend("Assignment_3.controller.Main", {
-        onSubmit: function () {
-            var oModel = this.getView().byId("userForm").getModel();
-            var oFormData = oModel.getData();
-            console.log("Submitted Data:", oFormData);
-        }
+  
+    return Controller.extend("com.ui5learners.controller.Main", {
+      onInit: function() {
+        // Controller initialization logic
+      },
+  
+      onSubmit: function() {
+        // Handle form submission
+        alert("Form submitted!");
+      }
     });
-});
+  });
+  
