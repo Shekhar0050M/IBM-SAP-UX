b2bf21e7bf01027e69c05019f847bc5daf134133
Shekhar0050M <shekhar0050m@gmail.com>
2023-11-21 09:15:17 +0530

    block ui not visible

---
+++ Assignment_4/controller/Input.fragment.js
@@ -0,0 +1,8 @@
+sap.ui.jsfragment("com.ibm.simpleapp.fragments.InputJS", {
+    createContent: function(){
+        var oInput = new sap.m.Input({
+            value: "Fragment JS",
+        });
+        return oInput;
+    }
+})
